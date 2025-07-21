4e894c0e4166109abd5e1e8b7e95236b48fe5d8c
Mani-420 <tahirabdulrehman46@gmail.com>
2025-07-21 19:20:45 +0500

    Page

---
+++ nextjs/src/app/blog/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Blog() {
+  return (
+    <div className="m-3 flex flex-col items-center justify-center min-h-screen">
+      This is Blog Page
+    </div>
+  );
+}
+
+export default Blog;

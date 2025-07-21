b1727e2d97cc865a3e4ac8342f2b637b9ed8d370
Mani-420 <tahirabdulrehman46@gmail.com>
2025-07-21 18:32:33 +0500

    Pages

---
+++ nextjs/src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="m-3 text-3xl font-bold flex flex-col items-center justify-center min-h-screen">
+      This is About Page{' '}
+    </div>
+  );
+}
+
+export default About;
---
+++ nextjs/src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Profile() {
+  return (
+    <div className="m-3 flex text-3xl font-bold flex-col items-center justify-center min-h-screen">
+      This is Profile Page{' '}
+    </div>
+  );
+}
+
+export default Profile;

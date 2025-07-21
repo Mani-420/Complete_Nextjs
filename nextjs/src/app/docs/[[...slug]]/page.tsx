d3f2de8f1daba43055597b06eca1d4b4b183b077
Mani-420 <tahirabdulrehman46@gmail.com>
2025-07-21 19:48:03 +0500

    Catch All

---
+++ nextjs/src/app/docs/[[...slug]]/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+async function Docs({ params }: { params: Promise<{ slug: string[] }> }) {
+  const { slug } = await params;
+  if (slug.length === 2) {
+    return (
+      <h1>
+        Docs Page for feature {slug[0]} and concept {slug[1]}
+      </h1>
+    );
+  } else if (slug.length === 1) {
+    return <h1>Docs Page for feature {slug[0]}</h1>;
+  }
+  return <div>Docs Home Page for {slug.join('/')}</div>;
+}
+
+export default Docs;

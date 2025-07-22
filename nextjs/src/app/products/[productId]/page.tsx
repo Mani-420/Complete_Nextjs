d8634aa64300d0a7d2b89fc1c54074fd812297d8
Mani-420 <tahirabdulrehman46@gmail.com>
2025-07-22 16:14:45 +0500

    MetaData

--- nextjs/src/app/products/[productId]/page.tsx
+++ nextjs/src/app/products/[productId]/page.tsx
@@ -1,10 +1,21 @@
+import { Metadata } from 'next';
 import React from 'react';
 
-async function ProductDetails({
-  params,
-}: {
+type Props = {
   params: Promise<{ productId: string }>;
-}) {
+};
+
+export const generateMetadata = async ({
+  params
+}: Props): Promise<Metadata> => {
+  const id = (await params).productId;
+  return {
+    title: `Product Details for ${id}`,
+    description: `Details of product with ID ${id}`
+  };
+};
+
+async function ProductDetails({ params }: Props) {
   const productId = (await params).productId;
   return (
     <div className="m-3 flex flex-col items-center justify-center min-h-screen">

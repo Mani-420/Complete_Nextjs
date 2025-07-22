85f2290299648eb27149b888edeb8017c2de2e44
Mani-420 <tahirabdulrehman46@gmail.com>
2025-07-22 15:14:10 +0500

    Not Found

---
+++ nextjs/src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div className="font-bold text-5xl flex flex-col items-center min-h-screen justify-center">
+      Not Found
+    </div>
+  );
+}
+
+export default NotFound;

dddf9e407fc9c6635ad3c9896524bf18b21fb4b9
Mani-420 <tahirabdulrehman46@gmail.com>
2025-07-25 23:11:47 +0500

    Middleware

---
+++ route-handlers/src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  //   return NextResponse.redirect(new URL('/', request.url));
+  const response = NextResponse.next();
+  const themePreference = request.cookies.get('theme');
+  if (themePreference) {
+    response.cookies.set('theme', 'dark');
+  }
+  return response;
+}
+
+export const config = {
+  matcher: '/profile'
+};

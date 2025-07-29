a7dbcceca229b60be31c51fff4db7098b22267bf
Mani-420 <tahirabdulrehman46@gmail.com>
2025-07-29 16:00:33 +0500

    Refactor ClientRoutePage and add ImageSlider component

--- rendering-comp/src/app/client-route/page.tsx
+++ rendering-comp/src/app/client-route/page.tsx
@@ -4,17 +4,17 @@ import React from 'react';
 // import Slider from "react-slick";
 // import "slick-carousel/slick/slick.css";
 // import "slick-carousel/slick/slick-theme.css";
-import { useTheme } from '@/components/theme-provider';
+// import { useTheme } from '@/components/theme-provider';
 //
 export default function ClientRoutePage() {
-  const theme = useTheme();
+  // const theme = useTheme();
   // const settings = {
   //   dots: true,
   // };
   return (
-    <div style={{ color: theme.colors.primary }}>
-      <h1>Client route page</h1>
-    </div>
+    <h1>Client route page</h1>
+    // <div style={{ color: theme.colors.primary }}>
+    // </div>
     // <div className="image-slider-container">
     //   <Slider {...settings}>
     //     <div>
